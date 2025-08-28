a2ebfadedc85e14409bbd5ef8f57520282b0274a
marcin <marcin.konowalczyk@canonical.com>
2025-08-28 17:23:57 +0100

    geturl_inlined

---
+++ src/geturl/geturl_inlined.py
@@ -0,0 +1,82 @@
+"""
+Micro inlined version of geturl with all the imports for easy copy-pasting.
+"""
+
+import urllib
+import urllib.error
+import urllib.parse
+import urllib.request
+from collections.abc import Mapping
+from typing import Optional
+
+
+# geturl from https://github.com/lczyk/geturl 0.4.5
+def geturl(
+    url: str,
+    params: Optional[Mapping[str, object]] = None,
+    headers: Optional[Mapping[str, str]] = None,
+) -> tuple[int, bytes]:
+    """Make a GET request to a URL and return the response and status code."""
+
+    if params is not None:
+        if "?" in url:
+            params = dict(params)  # make a modifiable copy
+            existing_params = urllib.parse.parse_qs(urllib.parse.urlparse(url).query)
+            for p_key, p_value in existing_params.items():
+                if p_key in params:
+                    # If the key is already in params, don't overwrite it
+                    continue
+                params[p_key] = p_value
+            url = url.split("?")[0]
+        url = url + "?" + urllib.parse.urlencode(params)
+
+    request = urllib.request.Request(url)
+    if headers is not None:
+        for h_key, h_value in headers.items():
+            request.add_header(h_key, h_value)
+
+    try:
+        with urllib.request.urlopen(request) as r:
+            code = r.getcode()
+            res = r.read()
+
+    except urllib.error.HTTPError as e:
+        code = e.code
+        res = e.read()
+
+    assert isinstance(code, int), "Expected code to be int."
+    assert isinstance(res, bytes), "Expected response to be bytes."
+
+    return code, res
+
+
+__license__ = """
+Copyright 2024 Marcin Konowalczyk
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+1.  Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+2.  Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+
+3.  Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
